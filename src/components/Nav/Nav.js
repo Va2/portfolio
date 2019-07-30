b6a0db0280f7720eee5b9bcd5e0bc6b21817dcef
Va2 <vadim.brieven@gmail.com>
2019-07-30 12:23:08 +0200

    fix Nav component box-shadow

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,14 +8,14 @@ const styles = {
 
 class Nav extends Component {
     state = {
-        bgColor: ''
+        topPosition: ''
     }
 
     listenScrollEvent = e => {
         if (window.scrollY > 50) {
-            this.setState({ bgColor: 'white' })
+            this.setState({ topPosition: "scrollOn" });
         } else {
-            this.setState({ bgColor: '' })
+            this.setState({ topPosition: "" });
         }
     }
 
@@ -25,54 +25,56 @@ class Nav extends Component {
 
     render() {
         return (
-            <div className="container">
-                <nav
-                    className={
-                        this.state.bgColor === "white"
-                        ? "navbar fixed-top navbar-expand-md navbar-light bg-white shadow-sm"
-                        : "navbar fixed-top navbar-expand-md navbar-light"
-                    }
-                    style={{ ...styles }}
-                >
-                    <a className="navbar-brand text-uppercase" /*href="#"*/>
-                        Vadim Brieven
-                    </a>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarNavAltMarkup"
-                        aria-controls="navbarNavAltMarkup"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon" />
-                    </button>
-                    <div
-                        className="collapse navbar-collapse justify-content-end"
-                        id="navbarNavAltMarkup"
-                    >
-                        <div className="navbar-nav">
-                            <a className="nav-item nav-link active" href="#header">
-                                Home <span className="sr-only">(current)</span>
-                            </a>
-                            <a className="nav-item nav-link" href="#experience">
-                                Experiences
-                            </a>
-                            <a className="nav-item nav-link" href="#projects">
-                                Projects
-                            </a>
-                            <a className="nav-item nav-link" href="#skills">
-                                Skills
-                            </a>
-                            <a className="nav-item nav-link" href="#contact">
-                                Contact
-                            </a>
-                        </div>
-                    </div>
-                </nav>
-
-            </div>
+          <div className="container">
+            <nav
+              className={
+                this.state.topPosition === "scrollOn"
+                  ? "navbar fixed-top navbar-expand-md bg-white navbar-light"
+                  : "navbar fixed-top navbar-expand-md navbar-light box-shadow-none"
+              }
+              style={{ ...styles }}
+            >
+              <a className="navbar-brand text-uppercase" /*href="#"*/>
+                Vadim Brieven
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+              <div
+                className="collapse navbar-collapse justify-content-end"
+                id="navbarNavAltMarkup"
+              >
+                <div className="navbar-nav">
+                  <a
+                    className="nav-item nav-link active"
+                    href="#header"
+                  >
+                    Home <span className="sr-only">(current)</span>
+                  </a>
+                  <a className="nav-item nav-link" href="#experience">
+                    Experiences
+                  </a>
+                  <a className="nav-item nav-link" href="#projects">
+                    Projects
+                  </a>
+                  <a className="nav-item nav-link" href="#skills">
+                    Skills
+                  </a>
+                  <a className="nav-item nav-link" href="#contact">
+                    Contact
+                  </a>
+                </div>
+              </div>
+            </nav>
+          </div>
         );
     }
 }
